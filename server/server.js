a564299579e177810ec74f2ac36026e949db12a1
Roger Mannby <roger.mannby@one.se>
2018-01-17 20:03:04 +0100

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+    socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcom to the chat group',
+      createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New user joined',
+      createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -21,6 +33,12 @@ io.on('connection', (socket) => {
       test: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   test: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
   });
 
   socket.on('disconnect', () => {
